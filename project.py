2eb7d37b19bc71a57604b72b837c60938aec2878
Vahdet Unal <vahdetunal58@gmail.com>
2019-04-18 08:14:02 +0300

    feat: Add google login

--- project.py
+++ project.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask import session as login_session
+from flask import make_response
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, City, Site
 
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+import httplib2
+import json
+import requests
 import random, string
+
 
 app = Flask(__name__)
 
 
-# Create and store a state token to prevent cross site request forgery attacks.
-def generate_state_token():
-    characters = string.ascii_uppercase + string.digits
-    state = ''.join(random.choice(characters) for i in range(32))
-    return state
-
-@app.route('/login')
-def show_login():
-    state = generate_state_token()
-    login_session['state'] = state
-    return render_template('login.html', STATE=state)
+CLIENT_ID = json.loads(
+            open('client_secrets.json', 'r').read())['web']['client_id']
 
 # Create the engine for DB connection
 engine = create_engine('sqlite:///historicalsites.db')
@@ -29,6 +28,106 @@ engine = create_engine('sqlite:///historicalsites.db')
 # Create a DB session
 Session = sessionmaker(bind=engine)
 session = Session()
+
+# Create and store a state token to prevent cross site request forgery attacks.
+def generate_state_token():
+    characters = string.ascii_uppercase + string.digits
+    state = ''.join(random.choice(characters) for i in range(32))
+    return state
+
+
+@app.route('/login')
+def show_login():
+    state = generate_state_token()
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
+
+
+# Connect the user using Google oAuth
+@app.route('/gconnect', methods=['POST', 'GET'])
+def gconnect():
+    # State token validation against cross site forgery attacks.
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Get the one time access token
+    code = request.data
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'
+           .format(access_token))
+    h = httplib2.Http()
+    # This request returns a bytes object and needs to be decoded.
+    result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    google_id = credentials.id_token['sub']
+    if result['user_id'] != google_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check if the user is already logged in.
+    stored_access_token = login_session.get('access_token')
+    stored_google_id = login_session.get('google_id')
+    if stored_access_token is not None and google_id == stored_google_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['google_id'] = google_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print("done!")
+    return output
 
 
 # Show all cities
